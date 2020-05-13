1b3922937dca9a010d381e4203d183ff129c3dfe
Emmanuel MARIE <emmanuel.marie@qlapoti.fr>
2020-05-13 18:32:04 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:new, :index, :show, :create] do
+    resources :reviews, only: [:create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 # db/seeds.rb
 puts "Cleaning database..."
 Restaurant.destroy_all
 
-      # t.string :name
-      # t.string :address
-      # t.string :phone_number
-      # t.string :category
-
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "+33 6 83 03 38 93", category: "chinese" }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "+33 7 83 03 38 93", category: "italian" }
 
-[ dishoom, pizza_east ].each do |attributes|
+# dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "+33 6 83 03 38 93", category: "chinese" }
+# pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "+33 7 83 03 38 93", category: "italian" }
+
+# [ dishoom, pizza_east ].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+
+italian = { name: "Vulcano", address: "Paris, 14ème", category: "italian" }
+chinese = { name: "Nems Party", address: "Paris, 13ème", category: "chinese" }
+japanese = { name: "Sushi Folie ", address: "Paris, 15ème", category: "japanese" }
+french = { name: "A la bonne fourchette", address: "Paris, 19ème", category: "french" }
+belgian = { name: "Tintin", address: "Bruxelles", category: "belgian" }
+
+[italian, chinese, japanese, french, belgian].each do |attributes|
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
 end
+
+# review1 = Review.create!(content: "yummy yummy", rating: 2)
+# review2 = Review.create!(content: "awesome", rating: 4)
+# review3 = Review.create!(content: "top!!", rating: 5)
+# review4 = Review.create!(content: "yoo", rating: 2)
+# review5 = Review.create!(content: "nice", rating: 3)
+
+
 puts "Finished!"
